857ae5835c4ffe08f56516850df9fb2c7ae97ce7
Sara <sara_banaeirad@hotmail.com>
2020-10-29 14:45:48 -0700

    change background image and styles

--- src/component/Home.js
+++ src/component/Home.js
@@ -2,12 +2,15 @@ import React from 'react';
 import logo from '../assets/images/logo_transparent.png';
 import { Link } from "react-router-dom";
 function Home() {
-
     return (
     <div className="home">
-        <img className='logo' src={logo} alt='logo' ></img> 
-        <h1>Daily Caloric Needs</h1>
-        <Link className="start-button" to="/calculator">Click here to start</Link>
+        <div>
+            <img className='logo' src={logo} alt='logo' ></img> 
+        </div>
+        <div className="home-text">
+            <h1>Daily Caloric Needs</h1>
+            <Link className="start-button" to="/calculator">Click here to start</Link>
+        </div>
     </div>
     );
 }
