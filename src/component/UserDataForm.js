0e500d109b95ea54218241b1945313bdb7f4c8e7
Sara <sara_banaeirad@hotmail.com>
2020-10-29 15:36:09 -0700

    imprial calculation is done

--- src/component/UserDataForm.js
+++ src/component/UserDataForm.js
@@ -62,7 +62,7 @@ const UserDataForm = (props) => {
         validationSchema={fromInputSchemaForMetric}
         onSubmit={(values, {setSubmitting, resetForm}) => {         
             setTimeout(()=>{
-                const res = calculate(values);
+                const res = calculate(values, isMetric);
                 setResult(res);
                 setSubmitting(false);
         },1000)
@@ -76,16 +76,16 @@ const UserDataForm = (props) => {
                 <option name="male" value="male" id="male">Male</option>
             </CustomSelect>   
             <CustomSelect label="Activity Level" name="activityLevel">
-                <option name="sedentary" value="1.2" id="sedentary">Sedentary</option>
-                <option name="lightly-active" value="1.375" id="lightly-active">Lightly Active</option>
-                <option name="moderately-active" value="1.5" id="moderaely-active">Moderately Active</option>
-                <option name="very-active" value="1.725" id="very-active">Very Active</option>
-                <option name="extremely-active" value="1.9" id="extremely-active">Extremely Active</option>
+                <option name="sedentary" value="1.2" id="sedentary">Sedentary - little or no exercise</option>
+                <option name="lightly-active" value="1.375" id="lightly-active">Lightly Active - light exercise 1-3 days/week</option>
+                <option name="moderately-active" value="1.5" id="moderaely-active">Moderately Active - moderate exercise 3-5 days/week</option>
+                <option name="very-active" value="1.725" id="very-active">Very Active - hard exercise 6-7 days a week</option>
+                <option name="extremely-active" value="1.9" id="extremely-active">Extremely Active - very hard exercise or 2x training</option>
             </CustomSelect>       
             <CustomNumberInput name="age" type="number" placeholder="Age" />      
-            <CustomNumberInput name="height" type="number" placeholder={isMetric?"Height (Cm)":"Height (Feet)"} />
-            <CustomNumberInput style={{display: isMetric && 'none'}} name="inch" type="number" placeholder="Height (Inch)" />    
-            <CustomNumberInput name="weight" type="number" placeholder={isMetric?"Weight (Kg)":"Weight (lb)"}/>
+            <CustomNumberInput name="height" type="number" placeholder={isMetric?"Height (cm)":"Height (feet)"} />
+            <CustomNumberInput style={{display: isMetric && 'none'}} name="inch" type="number" placeholder="Height (inch)" />    
+            <CustomNumberInput name="weight" type="number" placeholder={isMetric?"Weight (kg)":"Weight (lb)"}/>
             <button type="submit" className="submit-button">{props.isSubmitting?"Loading...":"Submit"}</button>
         </Form>
         )}         
--- src/component/formula.js
+++ src/component/formula.js
@@ -1,15 +1,21 @@
-export const calculate = (values) => {
-    console.log(values)
+export const calculate = (values, isMetric) => {
+
+    const height = (isMetric ? values.height : (values.height*12 + (typeof values.inches == 'number' && values.inches)) * 2.54);
+    const weight = (isMetric ? values.weight : values.weight*0.45);
+    const activityLevel = Number(values.activityLevel);
+    
     let BMR;
     let caloriesPerDay;
+    
+    // Mifflin-St Jeor Equation:
     if (values.sex==="female"){
-        BMR =  (10 * values.weight) + (6.25 * values.height) - (5 * values.age) - 161;
-        caloriesPerDay = BMR * Number(values.activityLevel);
+        BMR =  (10 * weight) + (6.25 * height) - (5 * values.age) - 161;
+        caloriesPerDay = BMR * activityLevel;
     }
     else{
-        BMR =  (10 * values.weight) + (6.25 * values.height) - (5 * values.age) + 5;
-        caloriesPerDay = BMR * Number(values.activityLevel);
+        BMR =  (10 * weight) + (6.25 * height) - (5 * values.age) + 5;
+        caloriesPerDay = BMR * activityLevel;
     }
-    const result = {"BMR":BMR, "CaloriesPerDay":Math.round(caloriesPerDay)}
+    const result = {"BMR":Math.round(BMR), "CaloriesPerDay":Math.round(caloriesPerDay)}
     return result
 }
